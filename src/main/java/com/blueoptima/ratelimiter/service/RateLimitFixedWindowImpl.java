b9e48ce886cbd111fc754e2383437a43f9aa32f0
Sahil Singla <sahil.singla@rategain.com>
2020-06-09 15:38:25 +0530

    Implemented improved fixed algo with redis

--- src/main/java/com/blueoptima/ratelimiter/service/RateLimitFixedWindowImpl.java
+++ src/main/java/com/blueoptima/ratelimiter/service/RateLimitFixedWindowImpl.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 /**
  * Much better than naive Fixed window algorithm
  * Traffic burst is smoothened by extrapolating the number of requests from the previous window.
@@ -23,21 +26,50 @@ public class RateLimitFixedWindowImpl implements RateLimitService{
 	private JedisPool jedisPool;
 
 	@Override public boolean allowRequest(String userId, Long apiId, Integer maxRateLimit) {
+		int totalCount = 0;
+		LOGGER.info("maxRateLimit: " + maxRateLimit);
 		try(Jedis jedis = jedisPool.getResource()) {
-			String key = userId + "_id:" + apiId;
+			final int millisToMinute = 60 * 1000;
+			final int millisToSec = 1000;
 
-			jedis.set(key, "32");
-			jedis.expire(key, 2);
-			long start = System.nanoTime();
-			String cachedResponse = jedis.get(key);
-			LOGGER.info(cachedResponse + " in time: " + (System.nanoTime() - start));
-			Thread.sleep(1900);
-			final String s = jedis.get(key);
-			LOGGER.info(s);
-		}catch (InterruptedException e){
-			e.printStackTrace();
+			// Remove milliseconds from the time
+			final long currentTime = System.currentTimeMillis();
+			final long currentTimeInSec =(currentTime / millisToSec) * millisToSec;
+			final long currentTimeInMinute = (currentTime / millisToMinute) * millisToMinute;
+
+			// Check the timestamp of previous window
+			final long prevTimeInMinute = currentTimeInMinute - millisToMinute;
+
+			String currentKey = userId + "_" + apiId + "_" + currentTimeInMinute;
+			String prevKey = userId + "_" + apiId + "_" + prevTimeInMinute;
+
+			String currentCountStr = jedis.get(currentKey);
+			int currentCount  = 0;
+
+			if (currentCountStr != null)
+				currentCount = Integer.parseInt(currentCountStr);
+
+			String prevCountStr = jedis.get(prevKey);
+			int extrapolatedPrevCount = 0;
+
+			if (prevCountStr != null){
+				int secondsOfPrevWindow = (60000 - (int)(currentTimeInSec - currentTimeInMinute))/1000;
+				int prevCount = Integer.parseInt(prevCountStr);
+				extrapolatedPrevCount = (prevCount * secondsOfPrevWindow)/60;
+			}
+
+			totalCount = 1 + currentCount + extrapolatedPrevCount;
+
+			jedis.incr(currentKey);
+
+			// Key didn't exist before
+			if (currentCountStr == null) {
+				jedis.expire(currentKey, 120);
+			}
+
+			LOGGER.info("Count in the current window + : " + 1+currentCount);
+			LOGGER.info("Extrapolated count from the previous window: " + extrapolatedPrevCount);
 		}
-
-		return true;
+		return totalCount <= maxRateLimit;
 	}
 }
